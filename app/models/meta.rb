1747f2d68901f23e71fb18eb62b37693ccb8fe60
Karen <karen@mercosys.(none)>
2013-02-06 15:07:30 -0200

    metas-setor

--- app/models/meta.rb
+++ app/models/meta.rb
@@ -3,5 +3,15 @@ class Meta < ActiveRecord::Base
   has_many :meta_detalhes, :dependent => :destroy
 
   validates :moeda, :periodo_final, :periodo_inicio, :status, :presence => true
+
+  before_save :finds
+
+  def finds
+
+  	#busca setor
+	  set = Setor.find_by_id(self.setor_id);
+	  self.setor_nome = set.nome.to_s unless self.setor_id.blank?
+	  
+	end
   
 end
